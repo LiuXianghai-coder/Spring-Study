6c3ba069b062562fd7eae4706efe328b39845f59
xhliu2 <xhliu2@amarsoft.com>
2021-11-10 16:11:59 +0800

    just commit

--- demo/src/test/java/com/example/demo/Solution.java
+++ demo/src/test/java/com/example/demo/Solution.java
@@ -32,7 +32,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(findTargetSumWays(new int[]{0,0,0,0,0,0,0,0,1}, 1
-        ));
+        System.out.println(findTargetSumWays(new int[]{0,0,0,0,0,0,0,0,1}, 1));
     }
 }
