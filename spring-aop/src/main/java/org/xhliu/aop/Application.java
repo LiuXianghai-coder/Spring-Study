783f40a6bae8b515dc618ca85b6336046110d842
xhliu2 <xhliu2@amarsoft.com>
2021-11-11 16:04:49 +0800

    add some files

--- spring-aop/src/main/java/org/xhliu/aop/Application.java
+++ spring-aop/src/main/java/org/xhliu/aop/Application.java
@@ -1,14 +1,19 @@
 package org.xhliu.aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.xhliu.aop.service.OrderService;
 import org.xhliu.aop.service.UserService;
 
 public class Application {
-    public static void main( String[] args ) {
+
+    private final static Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args ) {
         ApplicationContext context =
-                new ClassPathXmlApplicationContext("classpath:application-autoproxy.xml");
+                new ClassPathXmlApplicationContext("classpath:application-regex-advisor.xml");
 
 //        UserService userService = (UserService) context.getBean("userServiceProxy");
 //        userService.createUser("Xianghai", "Liu");
