43a90d42df9bf77ff280525091c112158d221583
Xianghai Liu <1900327840@qq.com>
2021-11-21 10:52:17 +0800

    Add Spring WebFlux Project

---
+++ spring-webflux/src/test/java/org/xhliu/springwebflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xhliu.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
