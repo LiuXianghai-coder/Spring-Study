a7f86ffe4c7d7a32c427d0c3fe79b301ee39cce0
XianghaiLiu <1900327840@qq.com>
2022-03-05 18:26:10 +0800

    Add Nacos To Spring Cloud Integrate Example

--- cloud-nacos/nacos-server/src/main/java/org/xhliu/nacosserver/NacosServerApplication.java
+++ cloud-nacos/nacos-server/src/main/java/org/xhliu/nacosserver/NacosServerApplication.java
@@ -2,15 +2,12 @@ package org.xhliu.nacosserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NacosServerApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(NacosServerApplication.class, args);
-        ConfigurableApplicationContext contextBean = context.getBean(ConfigurableApplicationContext.class);
-        System.out.println("contextBean=" + contextBean);
+        SpringApplication.run(NacosServerApplication.class, args);
     }
 
 }
--- cloud-nacos/nacos-server/src/main/java/org/xhliu/nacosserver/controller/NacosServerController.java
+++ cloud-nacos/nacos-server/src/main/java/org/xhliu/nacosserver/controller/NacosServerController.java
@@ -1,9 +1,10 @@
 package org.xhliu.nacosserver.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,20 +14,35 @@ import javax.annotation.Resource;
  * @author xhliu
  * @time 2022-02-27-下午7:50
  */
+@RefreshScope
 @RestController
-@RefreshScope(proxyMode = ScopedProxyMode.NO)
 public class NacosServerController {
+    private final static Logger log = LoggerFactory.getLogger(NacosServerController.class);
+
     @Resource
     private ConfigurableApplicationContext applicationContext;
 
+    // @NacosValue 注解不生效。。。。。
     @Value("${student.name:null}")
     private String name;
 
-    @GetMapping(path = "/name")
-    public String getName() {
-        String name1 = applicationContext.getEnvironment()
-                .getProperty("student.name");
+    @Value("${config2.name:null}")
+    private String config2Name;
 
-        return String.format("name=%s, name1=%s", name1, name1);
+    @Value("${config3.name:null}")
+    private String config3Name;
+
+    @Value("${config4.name:null}")
+    private String config4Name;
+
+    @GetMapping("/name")
+    public String getName() {
+        String name1 = applicationContext.getEnvironment().getProperty("student.name");
+        log.info("names = {}", name1);
+//        String name2 = applicationContext.getEnvironment().getProperty("config1.name");
+//        String name3 = applicationContext.getEnvironment().getProperty("config2.name");
+        return String.format("name=%s<br> config2ame=%s<br> config3ame=%s<br> config4ame=%s",
+                name, config2Name, config3Name, config4Name
+        );
     }
 }
