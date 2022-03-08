2cacd49e3080064374da1da09f8983d47805736a
XianghaiLiu <1900327840@qq.com>
2022-03-08 08:19:32 +0800

    Add Spring Cloud GateWay Example

---
+++ spring-gateway/src/main/java/org/xhliu/gateway/controller/SimpleController.java
@@ -0,0 +1,21 @@
+package org.xhliu.gateway.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xhliu
+ * @time 2022-03-07-下午9:33
+ */
+@RestController
+public class SimpleController {
+    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
+
+    @GetMapping(path = "/say")
+    public String say() {
+        log.info("[spring-cloud-gateway-service] say Hello");
+        return "[spring-cloud-gateway-service] say Hello";
+    }
+}
