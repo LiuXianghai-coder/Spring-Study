b9777e4ebbee3621767673743a96d2d81d174e61
xhliu2 <xhliu2@amarsoft.com>
2022-06-19 08:18:33 +0800

    Jackson Configuration

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,37 @@
 package com.example.demo;
 
+import com.example.demo.config.JsonConfig;
 import com.example.demo.controller.JustController;
+import com.example.demo.entity.Order;
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class DemoApplication {
     public static void main(String[] args) throws FileNotFoundException {
         ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-        JustController bean = context.getBean(JustController.class);
-        System.out.println(bean);
+        JsonConfig jsonConfig = context.getBean(JsonConfig.class);
+        Gson gson = jsonConfig.gson();
+
+        Order order = new Order();
+        order.setId(1);
+        order.setOrderCreatedDate(LocalDate.now());
+        order.setOrderCreatedDateTime(LocalDateTime.now());
+
+        System.out.println(gson.toJson(order));
+
+        String json = gson.toJson(order);
+
+        Order order1 = gson.fromJson(json, Order.class);
+        System.out.println(order1);
+        System.out.println(gson.toJson(order1));
     }
 }
---
+++ demo/src/main/java/com/example/demo/entity/Order.java
@@ -0,0 +1,62 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author xhliu
+ * @create 2022-06-16-11:07
+ **/
+public class Order {
+    private int id;
+    private String orderName;
+    private String orderDesc;
+    private LocalDate orderCreatedDate;
+    private LocalDateTime orderCreatedDateTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public String getOrderDesc() {
+        return orderDesc;
+    }
+
+    public void setOrderDesc(String orderDesc) {
+        this.orderDesc = orderDesc;
+    }
+
+    public LocalDate getOrderCreatedDate() {
+        return orderCreatedDate;
+    }
+
+    public void setOrderCreatedDate(LocalDate orderCreatedDate) {
+        this.orderCreatedDate = orderCreatedDate;
+    }
+
+    public LocalDateTime getOrderCreatedDateTime() {
+        return orderCreatedDateTime;
+    }
+
+    public void setOrderCreatedDateTime(LocalDateTime orderCreatedDateTime) {
+        this.orderCreatedDateTime = orderCreatedDateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [id=" + id + ", orderName=" + orderName + ", orderDesc=" + orderDesc + ", orderCreatedDate=" +
+                orderCreatedDate + ", orderCreatedDateTime=" + orderCreatedDateTime + "]";
+    }
+}
