96c74b173f4ee2dc11f1b95554d28599102bfb56
xhliu <1900327840@qq.com>
2025-02-25 13:25:39 +0800

    remove TreeSkiplist test

--- demo/src/main/java/com/example/demo/algorithm/TreeSkiplist.java
+++ demo/src/main/java/com/example/demo/algorithm/TreeSkiplist.java
@@ -39,41 +39,4 @@ public class TreeSkiplist extends Skiplist {
         }
         return true;
     }
-
-    public static void main(String[] args) {
-        String opList = FileUtil.readString(new File("/tmp/opList_1.txt"), StandardCharsets.UTF_8);
-        String valList = FileUtil.readString(new File("/tmp/valList_1.txt"), StandardCharsets.UTF_8);
-        String[] ops = opList.split(",");
-        String[] vals = valList.split(",");
-        Skiplist skiplist1 = new Skiplist();
-        Skiplist skiplist2 = new TreeSkiplist();
-        for (int i = 0; i < ops.length; i++) {
-            String op = ops[i];
-            int num = Integer.parseInt(vals[i].replaceAll("\\s+", "").replaceAll("\"", ""));
-            skiplist1.check();
-            if (StringUtils.equals("add", op)) {
-                System.out.println("skiplist." + op + "(" + num + ");");
-                skiplist1.add(num);
-                skiplist2.add(num);
-            } else if (StringUtils.equals("search", op)) {
-                System.out.println("skiplist." + op + "(" + num + ");");
-                boolean s1 = skiplist1.search(num);
-                boolean s2 = skiplist2.search(num);
-                if (s1 != s2) {
-                    skiplist1.printArr();
-                    skiplist1.search(num);
-                }
-                Assertions.assertEquals(s1, s2);
-            } else if (StringUtils.equals("erase", op)) {
-                System.out.println("skiplist." + op + "(" + num + ");");
-                boolean e1 = skiplist1.erase(num);
-                boolean e2 = skiplist2.erase(num);
-                if (e1 != e2) {
-                    skiplist1.erase(num);
-                    skiplist1.search(num);
-                }
-                Assertions.assertEquals(e1, e2);
-            }
-        }
-    }
 }
