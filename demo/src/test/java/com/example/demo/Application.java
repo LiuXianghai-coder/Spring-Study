b1653a3e84a9ad4a9012aa05bf63c91473682a91
xhliu <1900327840@qq.com>
2025-08-31 22:01:42 +0800

    SynchronousQueue Fair Test

--- demo/src/test/java/com/example/demo/Application.java
+++ demo/src/test/java/com/example/demo/Application.java
@@ -1,29 +1,52 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Assertions;
-
-import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.SynchronousQueue;
 
 /**
  * @author xhliu2
  **/
 public class Application {
 
-    static final long MOD = (int) 1e9 + 7;
+    static void blockingQueueTest() throws Throwable {
+        Thread[] producerTs = new Thread[Runtime.getRuntime().availableProcessors()];
+        Thread[] consumerTs = new Thread[Runtime.getRuntime().availableProcessors()];
+        final BlockingQueue<String> queue = new SynchronousQueue<>(true);
 
-    public int numTilings(int n) {
-        long[][] dp = new long[n + 1][4];
-        dp[0][3] = 1L;
-        for (int i = 1; i <= n; ++i) {
-            dp[i][0] = dp[i - 1][3];
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
-            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
-            dp[i][3] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][3]) % MOD;
+        CountDownLatch endLatch = new CountDownLatch(producerTs.length);
+        for (int i = 0; i < consumerTs.length; i++) {
+            consumerTs[i] = new Thread(() -> {
+                String pollVal;
+                try {
+                    pollVal = queue.take();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + "-consumer:" + pollVal);
+            });
+            consumerTs[i].start();
+            Thread.sleep(500);
         }
-        return (int) (dp[n][3] % MOD);
+
+        for (int i = 0; i < producerTs.length; i++) {
+            final int val = i;
+            producerTs[i] = new Thread(() -> {
+                try {
+                    queue.put(String.valueOf(val));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    endLatch.countDown();
+                }
+            });
+            producerTs[i].start();
+        }
+
+        endLatch.await();
     }
 
     public static void main(String[] args) throws Throwable {
-        Application app = new Application();
+        blockingQueueTest();
     }
 }
